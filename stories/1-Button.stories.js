5150bdddfeb8917f0141e598d6ecdee1f051ee4e
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2019-09-20 20:30:08 +0200

    Storybook init

---
+++ stories/1-Button.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Button} from "@storybook/react/demo";
+import {action} from "@storybook/addon-actions";
+
+export default {
+  title: "Button"
+};
+
+export const primary = () => (
+    <Button onClick={action("clicked")}>{`Button`}</Button>
+);
