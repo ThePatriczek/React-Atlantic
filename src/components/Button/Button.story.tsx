3bb407beb750bb9db75cfac48e94f9e9fd89796a
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-09-21 18:20:02 +0200

    Knobs addon added

--- src/components/Button/Button.story.tsx
+++ src/components/Button/Button.story.tsx
@@ -1,9 +1,18 @@
+import { boolean, text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { Button } from './Button';
 
 const stories = storiesOf('Components', module);
 
-stories.add('Button', () => <Button>{`Test`}</Button>, {
-  info: { inline: true }
-});
+stories.addDecorator(withKnobs);
+
+stories.add(
+  'Button',
+  () => (
+    <Button isDisabled={boolean('Disabled', false)}>
+      {text('Label', 'Test')}
+    </Button>
+  ),
+  { info: { inline: true } }
+);
