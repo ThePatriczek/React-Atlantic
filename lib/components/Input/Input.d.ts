46eabd7f3eb6e410e0132d1f5e4742176e16baac
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-09-27 12:51:50 +0200

    Docs

--- lib/components/Input/Input.d.ts
+++ lib/components/Input/Input.d.ts
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Size } from '../../types';
 import { IconName } from '../Icon';
 import { TextAreaProps } from './TextArea';
+
+export declare type AutoComplete = 'name' | 'honorific-prefix' | 'given-name' | 'additional-name' | 'family-name' | 'honorific-suffix' | 'nickname' | 'username' | 'new-password' | 'current-password' | 'one-time-code' | 'organization-title' | 'organization' | 'street-address' | 'address-line1' | 'address-line2' | 'address-line3' | 'address-level4' | 'address-level3' | 'address-level2' | 'address-level1' | 'country' | 'country-name' | 'postal-code' | 'cc-name' | 'cc-given-name' | 'cc-additional-name' | 'cc-family-name' | 'cc-number' | 'cc-exp' | 'cc-exp-month' | 'cc-exp-year' | 'cc-csc' | 'cc-type' | 'transaction-currency' | 'transaction-amount' | 'language' | 'bday' | 'bday-day' | 'bday-month' | 'bday-year' | 'sex' | 'url' | 'photo';
 export interface InputProps {
     value?: React.ReactText;
     defaultValue?: React.ReactText;
@@ -20,6 +22,8 @@ export interface InputProps {
     isLoading?: boolean;
     /** custom className */
     className?: string;
+    htmlType?: 'text' | 'email' | 'password';
+    autoComplete?: AutoComplete;
 }
 export declare const Input: React.FC<InputProps> & {
     TextArea: React.FC<TextAreaProps>;
