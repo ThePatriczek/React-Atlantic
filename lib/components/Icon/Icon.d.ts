7aced89a3729dc446847dcd887620b5650c46eb4
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-11-27 11:23:16 +0100

    Docs

--- lib/components/Icon/Icon.d.ts
+++ lib/components/Icon/Icon.d.ts
@@ -1,5 +1,6 @@
 import * as React from 'react';
-export declare type IconName = 'arrowUp' | 'arrowDoubleUp' | 'arrowRight' | 'arrowDoubleRight' | 'arrowDown' | 'arrowDoubleDown' | 'arrowLeft' | 'arrowDoubleLeft' | 'hamburger' | 'loading' | 'edit' | 'copy' | 'check' | 'users' | 'user' | 'upload' | 'trash' | 'search' | 'plus' | 'logout' | 'license' | 'key' | 'info' | 'import' | 'export' | 'eye' | 'eyeSlash' | 'close' | 'clear' | 'enter' | 'error' | 'warning' | 'checkCircle' | 'calendar';
+
+export declare type IconName = 'arrowUp' | 'arrowDoubleUp' | 'arrowRight' | 'arrowDoubleRight' | 'arrowDown' | 'arrowDoubleDown' | 'arrowLeft' | 'arrowDoubleLeft' | 'hamburger' | 'loading' | 'edit' | 'copy' | 'check' | 'users' | 'user' | 'upload' | 'trash' | 'search' | 'plus' | 'logout' | 'license' | 'key' | 'info' | 'import' | 'export' | 'eye' | 'eyeSlash' | 'close' | 'clear' | 'enter' | 'error' | 'warning' | 'checkCircle' | 'calendar' | 'calendarAlt' | 'question' | 'profile' | 'zoom' | 'home' | 'bookmarks' | 'tags' | 'star' | 'note' | 'monitor' | 'list' | 'listAlt' | 'history' | 'folder' | 'file' | 'fileAlt' | 'comment' | 'calculator' | 'book' | 'bookOpened';
 export interface IconProps {
     name: IconName;
     isRotating?: boolean;
