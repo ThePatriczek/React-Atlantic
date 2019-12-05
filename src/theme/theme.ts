70c4f25628f4f08ec5b0f9a9925e4a0ede065ce7
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2019-12-05 12:02:22 +0100

    theme colors updated

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,45 @@
 export const theme = {
   color: {
     default: `#f2f2f2`,
-    primary: `#1990ea`,
-    success: `#7cb305`,
-    info: `#1990ea`,
-    warning: `#ffa500`,
-    error: `#d9121a`,
     border: `#d9d9d9`,
-    black: `#000`,
-    white: `#fff`
+    primary: {
+      alpha: `#1990EA`,
+      beta: `#47A6EE`,
+      gamma: `#75BCF2`,
+      delta: `#A3D2F7`,
+      epsilon: `#D1E9FB`
+    },
+    success: {
+      alpha: `#7CB305`,
+      beta: `#96C237`,
+      gamma: `#B0D169`,
+      delta: `#CAE19B`,
+      epsilon: `#E5F0CD`
+    },
+    warning: {
+      alpha: `#FFA500`,
+      beta: `#FFB733`,
+      gamma: `#FFC966`,
+      delta: `#FFDB99`,
+      epsilon: `#FFEDCC`
+    },
+    error: {
+      alpha: `#D9121A`,
+      beta: `#E04147`,
+      gamma: `#E87075`,
+      delta: `#F0A0A3`,
+      epsilon: `#F8CFD1`
+    },
+    text: {
+      alpha: `#000`,
+      beta: `#777`,
+      gamma: `#FFF`
+    },
+    background: {
+      alpha: `#FFF`,
+      beta: `#F2F2F2`,
+      gamma: `#FFF`
+    }
   },
   padding: {
     xs: `2px`,
