1e0c8201d807df02fdda85d3c0405595322517d0
thepatriczek <patrik.szewczyk@gmail.com>
2020-02-05 14:55:04 +0100

    Tabs padding removed

--- src/components/Tabs/Tabs.style.ts
+++ src/components/Tabs/Tabs.style.ts
@@ -60,8 +60,6 @@ interface StyledTabsContentProps extends StyledTabsContainerProps {
 export const StyledTabsContent = styled.div<StyledTabsContentProps>`
   font-size: ${props => props.theme.font.size.md};
   font-family: ${props => props.theme.font.family};
-  padding: ${props => props.theme.padding.md} ${props => props.theme.padding.lg}
-    ${props => props.theme.padding.md} ${props => props.theme.padding.lg};
 
   ${props =>
     props.hasBackground &&
