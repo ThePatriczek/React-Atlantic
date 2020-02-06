ce607a8b3e038394b28ec53f1eeab412c2d6f3eb
Patrik Meixner <patrik.meixner98@gmail.com>
2020-02-06 14:48:08 +0100

    Transfer event hanlder - mouse down hotfix

--- src/components/Select/Transfer/Transfer.tsx
+++ src/components/Select/Transfer/Transfer.tsx
@@ -118,7 +118,7 @@ export const Transfer: FC<PropsWithChildren<TransferProps>> & {
 
   useEffect(() => {
     if (isOpen) {
-      window.addEventListener('onmousedown', onMouseDown);
+      window.addEventListener('mousedown', onMouseDown);
       window.addEventListener('keydown', onKeyDown);
     }
 
@@ -152,7 +152,7 @@ export const Transfer: FC<PropsWithChildren<TransferProps>> & {
     }
 
     return () => {
-      window.removeEventListener('onmousedown', onMouseDown);
+      window.removeEventListener('mousedown', onMouseDown);
       window.removeEventListener('keydown', onKeyDown);
     };
   }, [isOpen]);
