d03691b5c795bac3707936743c56ecc0e53a443a
Patrik Meixner <patrik.meixner98@gmail.com>
2020-02-12 14:47:50 +0100

    Timeline - added index test

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -13,8 +13,8 @@ export const Timeline: FC<PropsWithChildren<TimelineProps>> & {
   Item: FC<Readonly<ItemProps>>;
 } = (props): Readonly<ReactElement> => {
   const { children, onChange, index } = props;
-
   const { getFilteredChildren } = useComposition();
+
   const timelineItem = getFilteredChildren(children, Timeline.Item.displayName);
   const items: JSX.Element[] = [...timelineItem];
 
