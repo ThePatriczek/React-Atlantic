e24ba8c5b7144b89cd66a123413da13be47eeee5
patrik_meixner <patrik.meixner98@gmail.com>
2020-02-14 08:54:18 +0100

    Merge branch 'master' of /Users/patrik_meixner/WebstormProjects/ATLAS/React-Atlantic with conflicts.

---
+++ src/components/Timeline/Item/Item.tsx
@@ -0,0 +1,30 @@
+import React, { FC, PropsWithChildren, ReactElement } from 'react';
+import { useTimeline } from '../Context';
+import { StyledTimelineItem } from './Item.style';
+
+export interface ItemProps {
+  onClick?: () => void;
+  index?: Readonly<number>;
+  className?: Readonly<string>;
+}
+
+export const Item: FC<PropsWithChildren<ItemProps>> = (props): ReactElement => {
+  const { children, index, className } = props;
+  const { onChange } = useTimeline();
+
+  const onClick = () => {
+    props.onClick?.();
+
+    if (typeof index !== 'undefined') {
+      onChange(index);
+    }
+  };
+
+  return (
+    <StyledTimelineItem onClick={onClick} className={className}>
+      {children}
+    </StyledTimelineItem>
+  );
+};
+
+Item.displayName = `Item`;
