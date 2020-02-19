33bb9fb22014b152f68bde8e7f1402560d8efcda
Patrik Meixner <patrik.meixner98@gmail.com>
2020-02-19 13:23:17 +0100

    ...

--- src/components/Timeline/Item/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Item.style';
-export * from './Item';
--- src/components/Timeline/Timeline.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export interface StyledTimelineProps {}
-
-export const StyledTimelineContainer = styled.ul<StyledTimelineProps>`
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-`;
