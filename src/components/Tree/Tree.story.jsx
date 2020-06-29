ad51ed4531a498d98cbd6d21311cde0eddb6ee1c
Patrik Szewczyk <szewczyk@atlasgroup.cz>
2020-06-29 10:01:50 +0200

    tree with button story

--- src/components/Tree/Tree.story.jsx
+++ src/components/Tree/Tree.story.jsx
@@ -2,9 +2,9 @@ import { boolean, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { Tree } from './Tree';
-import { Typography } from '../Typography';
 import { action } from '@storybook/addon-actions';
 import { defaultValues } from '../../constants/defaultValues';
+import { Button, Icon, Typography } from '../';
 
 const stories = storiesOf('Tree', module);
 
@@ -26,6 +26,21 @@ stories.add('Playground', () => {
           {
             title: <Text>{`Node 1`}</Text>,
             children: [{ title: <Text>{`Node 1.1`}</Text> }]
+          },
+          {
+            title: (
+              <div>
+                <Button>
+                  <Icon name={'folder'} />
+                </Button>
+                <Text>{`Node 2`}</Text>
+              </div>
+            ),
+            children: [
+              {
+                title: <Text>{`Node 2.1`}</Text>
+              }
+            ]
           }
         ]}
       />
