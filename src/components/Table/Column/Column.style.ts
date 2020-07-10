d32df21f0b4a4d488810f7cc823bd577101a4fba
Martin Topolanek <topolanekmar@gmail.com>
2020-07-10 15:20:34 +0200

    fixed table borders

--- src/components/Table/Column/Column.style.ts
+++ src/components/Table/Column/Column.style.ts
@@ -1,13 +1,15 @@
 import styled, { css } from 'styled-components';
-import { StyledIcon } from '../../Icon/Icon.style';
 import { StyledSkeleton } from '../../Skeleton/Skeleton.style';
-import { StyledText } from '../../Typography/Text/Text.style';
 import { StyledTitle5 } from '../../Typography/Title/Title.style';
 
 export const StyledColumn = styled.td`
   padding: ${props => props.theme.padding.md};
+  border-left: 1px solid ${props => props.theme.color.border};
+  border-bottom: 1px solid ${props => props.theme.color.border};
 
-  border: 1px solid ${props => props.theme.color.border};
+  &:first-child {
+    border-left: none;
+  }
 
   ${StyledTitle5} {
     margin-top: 0;
@@ -24,16 +26,40 @@ export const StyledColumnHeader = styled.th<StyledColumnHeaderProps>`
   position: sticky;
   top: 0;
   padding: ${props => props.theme.padding.md};
-
-  background-color: ${props => props.theme.color.background.beta};
-
-  border-color: ${props => props.theme.color.border};
-  border-style: solid;
-  border-width: 1px 1px 0 1px;
-
-  font-weight: bolder;
   white-space: nowrap;
   z-index: 1;
+  background-color: ${props => props.theme.color.background.beta};
+  font-weight: 700;
+  
+  &:before {
+    content: "";
+    left: 0;
+    top: 0;
+    bottom: 0;
+    position: absolute;
+    border-left: 1px solid ${props => props.theme.color.border};
+  }
+  
+  &:after {
+    content: "";
+    bottom: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    border-bottom: 1px solid ${props => props.theme.color.border};
+  }
+  
+  &:first-child {
+    border-top-left-radius: ${props => props.theme.radius};
+    
+    &:before {
+      border-left: none;
+    }
+  }
+  
+  &:last-child {
+    border-top-right-radius: ${props => props.theme.radius};
+  }
 
   ${props =>
     props.isClickable &&
@@ -46,26 +72,16 @@ export const StyledColumnHeader = styled.th<StyledColumnHeaderProps>`
       }
     `}
   
-    ${props =>
-      props.isActive &&
-      css`
-        * {
-          color: ${props.theme.color.primary.alpha};
-        }
-      `}
+  ${props =>
+    props.isActive &&
+    css`
+      * {
+        color: ${props.theme.color.primary.alpha};
+      }
+    `}
 
   ${StyledSkeleton} {
     margin: 0;
-  }
-
-  :after {
-    content: '';
-    position: absolute;
-    left: 0;
-    bottom: -1px;
-    width: 100%;
-
-    border-bottom: 1px solid ${props => props.theme.color.border};
   }
 `;
 
--- src/components/Table/Row/Row.style.ts
+++ src/components/Table/Row/Row.style.ts
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
+import { StyledColumn } from '../Column/Column.style';
 
 export const StyledRow = styled.tr`
-  :nth-of-type(even) {
+  &:nth-of-type(even) {
     background-color: ${props => props.theme.color.background.gamma};
+  }
+
+  &:last-child {
+    ${StyledColumn} {
+      border-bottom: none;
+    }
   }
 `;
