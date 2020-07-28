b15648707dd0fdb7631704a34b9adf26d574576b
nikolasrnickova <nikola.srnickova@gmail.com>
2020-07-28 12:57:24 +0200

    progresscircle component

--- src/components/Progress/ProgressCircle/ProgressCircle.tsx
+++ src/components/Progress/ProgressCircle/ProgressCircle.tsx
@@ -1,20 +1,38 @@
 import React, { ReactElement } from 'react';
 
-interface ProgressProps {
-    percent: Readonly<number>;
-    type?: Readonly<Type>;
-    positionText?: Readonly<Position>;
-    className?: string;
-}
+export const ProgressCircle: React.FC<ProgressCircleProps> = (props) => {
+    const { progress } = props;
+    const totalDashOffset = 251;
+    const currentDashOffset =
+        totalDashOffset - progress * (totalDashOffset / 100);
+    let circleTwoType: Type = 'error';
 
-export const ProgressCircle: ProgressTypeComponent = memo(
-    (props: Readonly<ProgressProps>): Readonly<ReactElement> => {
-        
-
-        return (
-            
-        );
+    if (progress > 25) {
+        circleTwoType = 'warning';
     }
-);
+    if (progress > 50) {
+        circleTwoType = 'primary';
+    }
+    if (progress > 75) {
+        circleTwoType = 'success';
+    }
 
-
+    return (
+        <StyledLoadingCircle>
+            <StyledSVG height="100" width="100" className="svg">
+                <StyledSVGCircleOne cx="50" cy="50" r="40" fill="none" />
+                <StyledSVGCircleTwo
+                    cx="50"
+                    cy="50"
+                    r="40"
+                    fill="none"
+                    type={circleTwoType}
+                    dashOffset={currentDashOffset}
+                />
+            </StyledSVG>
+            <StyledLoadingCircleText key={progress} type={circleTwoType}>
+                {`${progress}%`}
+            </StyledLoadingCircleText>
+        </StyledLoadingCircle>
+    );
+};
