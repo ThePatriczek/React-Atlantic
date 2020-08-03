3e2194727ee07fd6bd0ceca2de926c4dc26a18a5
Patrik Szewczyk <szewczyk@atlasgroup.cz>
2020-08-03 13:45:15 +0200

    radio imported from cdxx

--- src/components/Radio/Button/Button.tsx
+++ src/components/Radio/Button/Button.tsx
@@ -1,76 +1,71 @@
-import React, { FC, PropsWithChildren, ReactElement } from 'react';
+import React, { FC, HTMLAttributes, PropsWithChildren, ReactElement } from 'react';
+import { useClickChange } from '../../../hooks/EventHandlers/useClickChange';
 import { Size } from '../../../types';
 import { useRadioGroup } from '../Context';
 import { RadioProps } from '../Radio';
+import { StyledRadioButtonInputHidden, StyledRadioButtonLabel, StyledRadioButtonSpan } from './Button.style';
 
-import { useClickChange } from '../../../hooks/EventHandlers/useClickChange';
-import {
-  StyledRadioButtonInputHidden,
-  StyledRadioButtonLabel,
-  StyledRadioButtonSpan
-} from './Button.style';
-
-export interface ButtonProps extends RadioProps {
-  size?: Size;
+export interface ButtonProps extends RadioProps, Omit<HTMLAttributes<HTMLLabelElement>, 'onChange'> {
+	isTransparent?: boolean;
+	size?: Size;
 }
 
-export const Button: FC<PropsWithChildren<ButtonProps>> = (
-  props
-): ReactElement => {
-  const {
-    isDefaultChecked,
-    isDisabled,
-    className,
-    children,
-    value,
-    size
-  } = props;
-  const { value: ctxVal, setValue: setCtxVal } = useRadioGroup();
-  const { onChangeClick: hookOnChange, isChecked } = useClickChange({
-    isDisabled,
-    isDefaultChecked,
-    deps: [props.isChecked],
-    isChecked: props.isChecked,
-    onChange: props.onChange
-  });
+export const Button: FC<PropsWithChildren<ButtonProps>> = (props): ReactElement => {
+	const {
+		isDefaultChecked,
+		isDisabled,
+		className,
+		children,
+		value,
+		isTransparent,
+		size,
+		// tslint:disable-next-line:variable-name
+		onChange: _onChange,
+		...labelProps
+	} = props;
+	const { value: ctxVal, setValue: setCtxVal } = useRadioGroup();
+	const { onChangeClick: hookOnChange, isChecked } = useClickChange({
+		isDisabled,
+		isDefaultChecked,
+		deps: [props.isChecked],
+		isChecked: props.isChecked,
+		onChange: props.onChange,
+	});
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    hookOnChange(e);
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		hookOnChange(e);
 
-    if (!isDisabled) {
-      if (value !== undefined) {
-        setCtxVal(value);
-      }
-    }
-  };
+		if (!isDisabled) {
+			if (value !== undefined) {
+				setCtxVal(value);
+			}
+		}
+	};
 
-  let checked: boolean | undefined = props.isChecked || isChecked;
+	let checked: boolean | undefined = props.isChecked || isChecked;
 
-  if (ctxVal !== undefined) {
-    checked = ctxVal === value;
-  }
+	if (ctxVal !== undefined) {
+		checked = ctxVal === value;
+	}
 
-  return (
-    <StyledRadioButtonLabel
-      isChecked={checked}
-      isDisabled={isDisabled}
-      className={`atlantic--radio_button ${className}`}
-      size={size}
-    >
-      <StyledRadioButtonInputHidden
-        onChange={onChange}
-        checked={checked}
-        disabled={isDisabled}
-        value={value}
-      />
+	return (
+		<StyledRadioButtonLabel
+			isChecked={checked}
+			isDisabled={isDisabled}
+			className={`atlantic--radio_button ${className}`}
+			size={size}
+			isTransparent={isTransparent}
+			{...labelProps}
+		>
+			<StyledRadioButtonInputHidden onChange={onChange} checked={checked} disabled={isDisabled} value={value} />
 
-      <StyledRadioButtonSpan isDisabled={isDisabled} size={size}>
-        {children}
-      </StyledRadioButtonSpan>
-    </StyledRadioButtonLabel>
-  );
+			<StyledRadioButtonSpan isDisabled={isDisabled} size={size}>
+				{children}
+			</StyledRadioButtonSpan>
+		</StyledRadioButtonLabel>
+	);
 };
 
 Button.defaultProps = {
-  size: 'medium'
+	size: 'medium',
 };
--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -5,93 +5,78 @@ import { Button, ButtonProps } from './Button';
 import { useRadioGroup } from './Context';
 import { Group, GroupProps } from './Group';
 import {
-  StyledRadioInputHidden,
-  StyledRadioInputShown,
-  StyledRadioLabel,
-  StyledRadioMark,
-  StyledRadioSpan
+	StyledRadioInputHidden,
+	StyledRadioInputShown,
+	StyledRadioLabel,
+	StyledRadioMark,
+	StyledRadioSpan,
 } from './Radio.style';
 
 export interface RadioProps {
-  isChecked?: boolean;
-  isDefaultChecked?: boolean;
-  isDisabled?: boolean;
-  onChange?: (isChecked: boolean) => void;
-  position?: HorizontalPosition;
-  /** custom className */
-  className?: string;
-  value?: any;
+	isChecked?: boolean;
+	isDefaultChecked?: boolean;
+	isDisabled?: boolean;
+	onChange?: (isChecked: boolean) => void;
+	position?: HorizontalPosition;
+	/** custom className */
+	className?: string;
+	value?: any;
 }
 
 export const Radio: React.FC<React.PropsWithChildren<RadioProps>> & {
-  Button: React.FC<ButtonProps>;
-  Group: React.FC<GroupProps>;
+	Button: React.FC<ButtonProps>;
+	Group: React.FC<GroupProps>;
 } = (props: React.PropsWithChildren<RadioProps>): React.ReactElement => {
-  const {
-    isDefaultChecked,
-    position,
-    isDisabled,
-    className,
-    children,
-    value
-  } = props;
-  const { value: ctxVal, setValue: setCtxVal } = useRadioGroup();
-  const { onChangeClick: hookOnChange, isChecked } = useClickChange({
-    isDisabled,
-    isDefaultChecked,
-    deps: [props.isChecked],
-    isChecked: props.isChecked,
-    onChange: props.onChange
-  });
+	const { isDefaultChecked, position, isDisabled, className, children, value } = props;
+	const { value: ctxVal, setValue: setCtxVal } = useRadioGroup();
+	const { onChangeClick: hookOnChange, isChecked } = useClickChange({
+		isDisabled,
+		isDefaultChecked,
+		deps: [props.isChecked],
+		isChecked: props.isChecked,
+		onChange: props.onChange,
+	});
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    hookOnChange(e);
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		hookOnChange(e);
 
-    if (!isDisabled) {
-      if (value !== undefined) {
-        setCtxVal(value);
-      }
-    }
-  };
+		if (!isDisabled) {
+			if (value !== undefined) {
+				setCtxVal(value);
+			}
+		}
+	};
 
-  let checked: boolean | undefined = props.isChecked || isChecked;
+	let checked: boolean | undefined = props.isChecked || isChecked;
 
-  if (ctxVal !== undefined) {
-    checked = ctxVal === value;
-  }
+	if (ctxVal !== undefined) {
+		checked = ctxVal === value;
+	}
 
-  return (
-    <StyledRadioLabel
-      isChecked={checked}
-      isDisabled={isDisabled}
-      className={className}
-      position={position}
-      isDefaultChecked={isDefaultChecked}
-    >
-      {position === 'right' && (
-        <StyledRadioSpan isDisabled={isDisabled}>{children}</StyledRadioSpan>
-      )}
+	return (
+		<StyledRadioLabel
+			data-id="panel-radio-group"
+			isChecked={checked}
+			isDisabled={isDisabled}
+			className={className}
+			position={position}
+			isDefaultChecked={isDefaultChecked}
+		>
+			{position === 'right' && <StyledRadioSpan isDisabled={isDisabled}>{children}</StyledRadioSpan>}
 
-      <StyledRadioInputShown isDisabled={isDisabled}>
-        <StyledRadioMark isDisabled={isDisabled} isChecked={checked}>
-          <StyledRadioInputHidden
-            onChange={onChange}
-            checked={checked}
-            disabled={isDisabled}
-            value={value}
-          />
-        </StyledRadioMark>
-      </StyledRadioInputShown>
+			<StyledRadioInputShown isDisabled={isDisabled}>
+				<StyledRadioMark isDisabled={isDisabled} isChecked={checked}>
+					<StyledRadioInputHidden onChange={onChange} checked={checked} disabled={isDisabled} value={value} />
+				</StyledRadioMark>
+			</StyledRadioInputShown>
 
-      {position === 'left' && (
-        <StyledRadioSpan isDisabled={isDisabled}>{children}</StyledRadioSpan>
-      )}
-    </StyledRadioLabel>
-  );
+			{position === 'left' && <StyledRadioSpan isDisabled={isDisabled}>{children}</StyledRadioSpan>}
+		</StyledRadioLabel>
+	);
 };
 
 Radio.defaultProps = {
-  position: 'left'
+	position: 'left',
 };
 
 Radio.Button = Button;
